219019a1f78d5435acae08bd50186f504083576a
RebecaRosenquist <104893301+RebecaRosenquist@users.noreply.github.com>
2025-06-23 19:06:50 +0200

    CleanUp2

--- VehicleApi/Program.cs
+++ VehicleApi/Program.cs
@@ -3,24 +3,6 @@ using VehicleApi.Data;
 using System.Text.Json.Serialization;
 
 
-// var builder = WebApplication.CreateBuilder(args);
-
-// builder.Services.AddDbContext<CarContext>(options =>
-//     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-// builder.Services.AddControllers()
-//     .AddJsonOptions(options =>
-//     {
-//         options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
-//         options.JsonSerializerOptions.WriteIndented = true;
-//     });
-
-// var app = builder.Build();
-
-// app.MapControllers();
-
-// app.Run();
-
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddCors(options =>
